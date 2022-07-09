4e86e42117a4affd096dea868d071937e1457ce5
aswanym <aswanymahendran222@gmail.com>
2022-07-09 11:14:32 +0530

    footer component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import './contact.css'
 import {AiFillMail} from 'react-icons/ai'
 import {AiOutlineWhatsApp} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mig3qbt', 'template_s6hw8sg', form.current, 'jOJWDtB1C_49v3EF5')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
+
   return (
     <section id='contact'>
       <h5>Get in touch</h5>
@@ -26,7 +42,7 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
 
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your full name' required/>
           <input type='email' name='email' placeholder='Your email' required/>
           <textarea name='message' rows='?' placeholder='Your message' required></textarea>
