04258ae3410de0c01e2c9cd13876b5d7971bcb63
aswanym <aswanymahendran222@gmail.com>
2022-07-12 21:24:19 +0530

    change social links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsStackOverflow} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__social'>
-        <a href='https://linkedin.com' target= '_blank'><BsLinkedin /></a>
-        <a href='https://github.com' target= '_blank'><FaGithub /></a>
-        <a href='https://dripple.com' target= '_blank'><BsStackOverflow /></a>
+        <a href='https://www.linkedin.com/in/aswany-m-1696281b4/' target= '_blank'><BsLinkedin /></a>
+        <a href='https://github.com/Aswanym' target= '_blank'><FaGithub /></a>
+        <a href='https://stackoverflow.com/users/18954373/aswany-mahendran' target= '_blank'><BsStackOverflow /></a>
     </div>
   )
 }
